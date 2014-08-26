fb0a996e629e5770dfacae05be5c66180d67cb16
Rob Ashton <robashton@codeofrob.com>
2014-08-26 08:46:25 +0100

    Exercise 4

---
+++ 3/Golf.hs
@@ -0,0 +1,22 @@
+module Golf where
+
+--skips :: [a] -> [[a]]
+--everyNths :: [a]
+--
+--skips :: [a] -> [[a]]
+----skips [] = []
+----skips y@(_:r) = [y] ++ skips r
+--skips xs = map (\x ->
+--
+--
+--
+--localMaxima :: [Integer] -> [Integer]
+--localMaxima [x,y,z]
+--  | x < y && y > z = [y]
+--  | otherwise = []
+--localMaxima (x:xs@(y:(z:_)))
+--  | x < y && y > z = y : localMaxima xs
+--  | otherwise = localMaxima xs
+--localMaxima _ = []
+
+-- Sod this, I was going to do them all long form and then find appropriate haskell functions but then I realised that this is something I'll never actually finihs..
---
+++ 4/Test.hs
@@ -0,0 +1,27 @@
+module Test where
+
+fun1 :: [Integer] -> Integer
+fun1 [] = 1
+fun1 (x:xs)
+  | even x = (x - 2) * fun1 xs
+  | otherwise = fun1 xs
+
+fun1' :: [Integer] -> Integer
+fun1' = product . map (\x -> x - 2) . filter even
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n
+  | even n = n + fun2 (n `div` 2)
+  | otherwise = fun2 (3 * n + 1)
+
+
+-- Honestly don't think this is much better
+-- What am I missing?
+tran :: Integer -> Integer
+tran x
+  | even x = x `div` 2
+  | otherwise = 3 * x + 1
+
+fun2' :: Integer -> Integer
+fun2' = sum . filter even . takeWhile (/=1) . iterate tran
