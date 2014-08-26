05dce0db84293f3468c419e9e97fb988f7cb81a0
Rob Ashton <robashton@codeofrob.com>
2014-08-26 16:33:40 +0100

    A few more idiomatic things

--- 4/Test.hs
+++ 4/Test.hs
@@ -16,12 +16,43 @@ fun2 n
   | otherwise = fun2 (3 * n + 1)
 
 
--- Honestly don't think this is much better
--- What am I missing?
-tran :: Integer -> Integer
-tran x
-  | even x = x `div` 2
-  | otherwise = 3 * x + 1
-
+-- I don't think he even wants those guard clauses?
 fun2' :: Integer -> Integer
-fun2' = sum . filter even . takeWhile (/=1) . iterate tran
+fun2' = sum . filter even . takeWhile (/=1) . iterate nextfn
+  where nextfn x
+          | even x = x `div` 2
+          | otherwise = 3 * x + 1
+
+
+-- Wow yuck, there has to be a tidier way of composing this
+data Tree a = Leaf
+            | Node Int (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+foldTree :: [a] -> Tree a
+foldTree xs = foldr insert Leaf xs
+  where insert :: a -> Tree a -> Tree a
+        insert x Leaf = Node 0 Leaf x Leaf
+        insert x (Node height left val right)
+          | treeHeight left <= treeHeight right =
+            let newLeft = insert x left
+           in (Node ((+1) $ treeHeight newLeft) newLeft val right)
+          | otherwise  =
+            let newRight = insert x right
+            in (Node height left val newRight)
+
+treeHeight :: Tree a -> Int
+treeHeight Leaf = 0
+treeHeight (Node height _ _ _) = height
+
+
+-- Ah, this is good
+xor :: [Bool] -> Bool
+xor = even . length . filter not
+
+-- This one is straightfoward at least
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr ((:) . f) []
+
+
+
