7ce29c88ab71fe9efb385174b624474b6dccad97
Rob Ashton <robashton@codeofrob.com>
2014-08-31 13:53:50 +0100

    first attempt at playing code golf, it'll do

--- 3/Golf.hs
+++ 3/Golf.hs
@@ -1,22 +1,36 @@
 module Golf where
 
---skips :: [a] -> [[a]]
---everyNths :: [a]
---
---skips :: [a] -> [[a]]
-----skips [] = []
-----skips y@(_:r) = [y] ++ skips r
---skips xs = map (\x ->
---
---
---
---localMaxima :: [Integer] -> [Integer]
---localMaxima [x,y,z]
---  | x < y && y > z = [y]
---  | otherwise = []
---localMaxima (x:xs@(y:(z:_)))
---  | x < y && y > z = y : localMaxima xs
---  | otherwise = localMaxima xs
---localMaxima _ = []
+import Data.List
 
--- Sod this, I was going to do them all long form and then find appropriate haskell functions but then I realised that this is something I'll never actually finihs..
+-- Blah blah blah explanation
+skip :: Int -> [a] -> [a]
+skip n = map snd . filter ((==0) . (`mod` n) . fst) . zip [1..]
+
+skips :: [a] -> [[a]]
+skips xs = map (`skip` xs) [1..length(xs)]
+
+-- Blah blah blah explanation (Cheeky rob is cheeky, drop 1 drop 2 CONCAT)
+maxima :: (Integer,Integer, Integer) -> [Integer]
+maxima (x,y,z) = if x < y && y > z then [y] else []
+
+generate :: [Integer] -> [(Integer, Integer, Integer)]
+generate xs = zip3 xs (drop 1 xs) (drop 2 xs)
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima = concat . map maxima . generate
+
+
+--- FUUUUUUUUUUUUUUUUUUUUUUU
+
+printline :: Int -> [Int] -> String
+printline line = map (\y -> if y > line then '*' else ' ')
+
+printogram :: [Int] -> String
+printogram xs = intercalate "\n" $ reverse $ "==========" : ['0'..'9'] : map (`printline` xs) [0..(maximum xs)]
+
+histogram :: [Integer] -> String
+histogram xs = printogram $ map (\n -> length $ filter (==n) xs) [0..9]
+
+testicles :: [Integer] -> IO ()
+testicles = putStr . histogram
+
